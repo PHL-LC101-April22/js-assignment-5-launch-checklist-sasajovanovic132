4617ff03b45f2acfb472c0abf4e01b80a08b40b5
sasajovanovic132 <75175528+sasajovanovic132@users.noreply.github.com>
2022-06-17 01:59:48 -0400

    script.js done

--- script.js
+++ script.js
@@ -1,16 +1,37 @@
-// Write your JavaScript code here!
+/ Write your JavaScript code here!
+
+
 
 window.addEventListener("load", function() {
 
-   let listedPlanets;
-   // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-   let listedPlanetsResponse;
-   listedPlanetsResponse.then(function (result) {
-       listedPlanets = result;
-       console.log(listedPlanets);
-   }).then(function () {
-       console.log(listedPlanets);
-       // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-   })
+    let listedPlanets;
+    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
+    let listedPlanetsResponse = myFetch();
+    listedPlanetsResponse.then(function (result) {
+     
+        listedPlanets = result;
+        console.log(listedPlanets);
+    }).then(function () {
+        console.log(listedPlanets);
+        addDestinationInfo(this.window.document, pickPlanet(listedPlanets).name, pickPlanet(listedPlanets).diameter, pickPlanet(listedPlanets).star, pickPlanet(listedPlanets).distance, pickPlanet(listedPlanets).moons, pickPlanet(listedPlanets).image) 
+    
+ 
+    })
+
+    
+    let pilotName = document.querySelector("input[name=pilotName]");
+    let copilotName = document.querySelector("input[name=copilotName]");
+    let fuelLevel= document.querySelector("input[name=fuelLevel]");
+    let cargoMass = document.querySelector("input[name=cargoMass]");
    
+   
+    formSubmission(window.document, pilotName, copilotName, fuelLevel,cargoMass);
+   
+
+
+  
+
 });
+
+
+
